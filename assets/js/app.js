5164443942c3338c973a805642d3ab2fd214ae3d
Flor Tello <flor.26071994@gmail.com>
2017-04-25 18:42:23 -0500

    Agregando contactos

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,18 +7,3 @@ function ocultar(){
 function mostrar(){
   document.getElementById("lugares").style.display='block';
 }
-
-
-var index = 0;
-
-slider();
-function slider(){
-  var sliders = document.getElementsByClassName('slider');
-  for (var i = 0; i < sliders.length; i++) {
-    sliders[i].style.display = "none";
-  }
-  index++;
-  if(index > sliders.length){index = 1;}
-    sliders[index-1].style.display = "block";
-    setTimeout(slider,1000);
-}
---
+++ assets/js/experince.js
@@ -0,0 +1,13 @@
+img.addEventListener('click',function(e){
+  mostrarModal(e.target.getAttribute('src'));
+  // document.getElementById('nextSpan').setAttribute('next-src',e.target.parentNode.nextSibling.firstChild.getAttribute('src'));
+});
+function mostrarModal(src){
+  document.getElementById('modal-img').setAttribute('src',src);
+  // document.getElementById('modal').style.display = 'block';
+  document.getElementById('modal').classList.toggle('OpenClose');
+  body.style.overflow = 'hidden';
+  var spanNext = document.createElement('span');
+  // spanNext.setAttribute('next-src',"assets/img/img-"+id+".jpg");
+  // figure.appendChild(spanNext);
+}
