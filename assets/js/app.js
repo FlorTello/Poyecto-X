6b3a3fce0074b4550f3f4178e9c92dadab3b226d
Flor Tello <flor.26071994@gmail.com>
2017-04-26 11:08:43 -0500

    Agregando el inputLike y almacenando en el localstorage

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,22 +9,6 @@ function mostrar(){
   document.getElementById("lugares").style.display='block';
 }
 
-var arr =[{name:"AREQUIPA", url:"assets/img/costa-arequipa.jpg", lugar:"Colca", video:"https://www.youtube.com/embed/N2FErj7Fj3M"},
-          {name:"ICA", url:"assets/img/costa-ica.jpg", lugar:"Huacachina", video:"https://www.youtube.com/embed/N2FErj7Fj3M"},
-          {name:"LIMA", url:"assets/img/costa-lima.jpg", lugar:"Barranco", video:"https://www.youtube.com/embed/N2FErj7Fj3M"},
-          {name:"PIURA", url:"assets/img/costa-piura.jpg", lugar:"Mancora", video:"https://www.youtube.com/embed/N2FErj7Fj3M"},
-          {name:"TRUJILLO", url:"assets/img/costa-trujillo.jpg", lugar:"Huanchaco", video:"https://www.youtube.com/embed/N2FErj7Fj3M"},
-          {name:"CHACHAPOYAS", url:"assets/img/selva-chachapoyas.jpg", lugar:"Kuelap", video:"https://www.youtube.com/embed/N2FErj7Fj3M"},
-          {name:"IQUITOS", url:"assets/img/selva-iquitos.jpg", lugar:"Quistococha", video:"https://www.youtube.com/embed/N2FErj7Fj3M"},
-          {name:"TARAPOTO", url:"assets/img/selva-tarapoto.jpg", lugar:"Camiri", video:"https://www.youtube.com/embed/N2FErj7Fj3M"},
-          {name:"AYACUCHO", url:"assets/img/sierra-ayacucho.jpg", lugar:"Carnavales", video:"https://www.youtube.com/embed/N2FErj7Fj3M"},
-          {name:"CUSCO", url:"assets/img/sierra-cusco.jpg", lugar:"Piedra de los 12 angulos", video:"https://www.youtube.com/embed/N2FErj7Fj3M"},
-          {name:"PASCO", url:"assets/img/sierra-pasco.jpg", lugar:"Oxapampa", video:"https://www.youtube.com/embed/N2FErj7Fj3M"},
-          {name:"JUNIN", url:"assets/img/sierra-junin.jpg", lugar:"Huagapo", video:"https://www.youtube.com/embed/N2FErj7Fj3M"}];
-
-
-arr.forEach(e => imprimir(e));
-
 function imprimir(elem){
   var figure = document.createElement("figure");
   figure.setAttribute("class", "effect-foto");
@@ -42,38 +26,45 @@ function imprimir(elem){
   figure.appendChild(img);
 
   var div = document.createElement('div');
-  var spanHeart = document.createElement('span');
+  var spanHeart = document.createElement('input');
+  spanHeart.setAttribute('type','checkbox');
   var spanComment = document.createElement('span');
   spanHeart.setAttribute('class','spanHeart fa fa-heart');
   spanComment.setAttribute('class','spanComment fa fa-comment');
+  if(elem.like == "true"){
+    spanHeart.classList.toggle('cambiarLike');
+    spanHeart.checked = true;
+  }
   div.appendChild(spanHeart);
-  // div.appendChild(spanComment);
+  div.appendChild(spanComment);
   figure.appendChild(div);
-
-  img.addEventListener('click',function(e){
-    mostrarModal(elem.video);
-  });
-
+  spanHeart.addEventListener('change',cambiarLike);
+  img.addEventListener('click',mostrarModal);
+   function cambiarLike(e){
+      this.classList.toggle('cambiarLike');
+      elem.like=(this.checked).toString();
+      localStorage.setItem('lugares',JSON.stringify(arr));
+    }
+  function mostrarModal(src){
+    document.getElementById('modal-video').setAttribute('src',elem.video);
+    document.getElementById('modal').classList.toggle('OpenClose');
+    document.getElementById("inicio").style.overflow = 'hidden';
+  }
   return figure;
 }
 document.getElementsByClassName('close')[0].addEventListener('click',agregarClass);
 
 function agregarClass(){
   this.parentNode.parentNode.classList.toggle('OpenClose');
-    document.getElementById("inicio").style.overflow = 'visible';
-
-}
-function mostrarModal(src){
-  document.getElementById('modal-video').setAttribute('src',src);
-  document.getElementById('modal').classList.toggle('OpenClose');
-  document.getElementById("inicio").style.overflow = 'hidden';
+  document.getElementById("inicio").style.overflow = 'visible';
 }
 
 
 window.addEventListener('load', function(e){
   e.preventDefault();
-  var ciudades = arr.forEach(function(e){
-    document.getElementsByClassName('grid')[0].appendChild(imprimir(e));
+  var dfm = document.createDocumentFragment();
+  arr.forEach(function(e){
+    dfm.appendChild(imprimir(e));
   })
-    return ciudades;
+  document.getElementsByClassName('grid')[0].appendChild(dfm);
 });
--- assets/js/datos.js
+++ assets/js/datos.js
@@ -10,3 +10,24 @@ if(!localStorage.getItem('usuarios')){
 else{
   usuario_LS = JSON.parse(localStorage.getItem('usuarios'));
 }
+var arr;
+var ArrayL=[{name:"AREQUIPA", url:"assets/img/costa-arequipa.jpg", lugar:"Colca", video:"https://www.youtube.com/embed/N2FErj7Fj3M",like:"false"},
+          {name:"ICA", url:"assets/img/costa-ica.jpg", lugar:"Huacachina", video:"https://www.youtube.com/embed/N2FErj7Fj3M",like:"false"},
+          {name:"LIMA", url:"assets/img/costa-lima.jpg", lugar:"Barranco", video:"https://www.youtube.com/embed/N2FErj7Fj3M",like:"false"},
+          {name:"PIURA", url:"assets/img/costa-piura.jpg", lugar:"Mancora", video:"https://www.youtube.com/embed/N2FErj7Fj3M",like:"false"},
+          {name:"TRUJILLO", url:"assets/img/costa-trujillo.jpg", lugar:"Huanchaco", video:"https://www.youtube.com/embed/N2FErj7Fj3M",like:"false"},
+          {name:"CHACHAPOYAS", url:"assets/img/selva-chachapoyas.jpg", lugar:"Kuelap", video:"https://www.youtube.com/embed/N2FErj7Fj3M",like:"false"},
+          {name:"IQUITOS", url:"assets/img/selva-iquitos.jpg", lugar:"Quistococha", video:"https://www.youtube.com/embed/N2FErj7Fj3M",like:"false"},
+          {name:"TARAPOTO", url:"assets/img/selva-tarapoto.jpg", lugar:"Camiri", video:"https://www.youtube.com/embed/N2FErj7Fj3M",like:"false"},
+          {name:"AYACUCHO", url:"assets/img/sierra-ayacucho.jpg", lugar:"Carnavales", video:"https://www.youtube.com/embed/N2FErj7Fj3M",like:"false"},
+          {name:"CUSCO", url:"assets/img/sierra-cusco.jpg", lugar:"Piedra de los 12 angulos", video:"https://www.youtube.com/embed/N2FErj7Fj3M",like:"false"},
+          {name:"PASCO", url:"assets/img/sierra-pasco.jpg", lugar:"Oxapampa", video:"https://www.youtube.com/embed/N2FErj7Fj3M",like:"false"},
+          {name:"JUNIN", url:"assets/img/sierra-junin.jpg", lugar:"Huagapo", video:"https://www.youtube.com/embed/N2FErj7Fj3M",like:"false"}];
+
+          if(!localStorage.getItem('lugares')){
+            localStorage.setItem('lugares',JSON.stringify(ArrayL));
+            arr = JSON.parse(localStorage.getItem('lugares'));
+          }
+          else{
+            arr = JSON.parse(localStorage.getItem('lugares'));
+          }
